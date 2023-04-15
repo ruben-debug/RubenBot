946954f605459c0ad20ef4cfd083c7da154e5b0a
ruben-debug <123603019+ruben-debug@users.noreply.github.com>
2023-04-15 17:08:28 -0600

    Bot grabs channel id and will respond back
    Will respond to the specific message in the channel id now, also broken token because theres a hidden token now

--- server.js
+++ server.js
@@ -10,20 +10,22 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Set up routes
-app.post("/send-message", function(req, res) {
-	// Get message from request body
-	const message = req.body.message;
+app.post("/send-message", function (req, res) {
+  // Get message from request body
+  const message = req.body.message;
 
-	// Send message to Discord bot
-	const channel = client.channels.cache.get("850958525776527373");
-	channel.send(message);
+  // Send message to Discord bot
+  const channel = client.channels.cache.get("850958525776527373");
+  channel.send(message);
 
-	// Send response back to client
-	res.status(200).send("Message sent!");
+  // Send response back to client
+  res.status(200).send("Message sent!");
 });
 
 // Start server and Discord bot
-app.listen(PORT, function() {
-	console.log("Server started on port " + PORT);
-	client.login("MTA5NjY0MDU3MDY4NTkyNzUyNQ.G0u9ko.Fd8FV2mOeLsZYzu31mHXwnLjG-NOltL2vAfwXY");
+app.listen(PORT, function () {
+  console.log("Server started on port " + PORT);
+  client.login(
+    "MTA5NjY0MDU3MDY4NTkyNzUyNQ.G0u9ko.Fd8FV2mOeLsZYzu31mHXwnLjG-NOltL2vAfwXY" // Token doesnt work, broken.
+  );
 });
